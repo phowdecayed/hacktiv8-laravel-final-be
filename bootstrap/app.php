33e82bba836b26023e5acbd07cdae1f4ec3d1ef2
rachmat <rachmat.septiana@jabarprov.go.id>
2025-07-29 15:08:08 +0700

    fix(cors): enable supports_credentials for api authentication
    Update axios dependency to latest patch version
    Add statefulApi middleware for api authentication

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
